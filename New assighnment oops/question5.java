774e9068c644f529847c0189f98e05ed089770db
krishnamvarshney3131 <krishnamvarshney3131@gmail.com>
2025-09-11 23:32:31 +0530

    Add files via upload

---
+++ New assighnment oops/question5.java
@@ -0,0 +1,48 @@
+
+
+import java.util.Scanner;
+
+class Student {
+    
+    private String name;
+    private int rollNumber;
+    private int marks;
+
+    
+    public Student(String name, int rollNumber, int marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+    
+    public char calculateGrade() {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 75) {
+            return 'B';
+        } else if (marks >= 50) {
+            return 'C';
+        } else if (marks >= 35) {
+            return 'D';
+        } else {
+            return 'F'; // Fail
+        }
+    }
+
+    
+    public void displayDetails() {
+        System.out.println("---- Student Report ----");
+        System.out.println("Name       : " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks      : " + marks);
+        System.out.println("Grade      : " + calculateGrade());
+        System.out.println("------------------------");
+    }
+}
+
+
+public class StudentReport {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.i
+
+
