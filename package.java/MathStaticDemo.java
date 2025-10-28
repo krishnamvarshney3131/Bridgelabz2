10c7fd8391628ba20a2243d839f56aa0c7f2411e
krishnamvarshney3131 <krishnamvarshney3131@gmail.com>
2025-10-28 21:58:48 +0530

    Add files via upload

---
+++ package.java/MathStaticDemo.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.*;
+
+public class MathStaticDemo {
+    public static void main(String[] args) {
+        System.out.println("Sqrt(16): " + sqrt(16));
+        System.out.println("Pow(2,5): " + pow(2,5));
+        System.out.println("Max(20,50): " + max(20,50));
+        System.out.println("Min(5,10): " + min(5,10));
+        System.out.println("Abs(-7): " + abs(-7));
+    }
+}
+
---
+++ package.java/ques6.java
@@ -0,0 +1,19 @@
+module collegeinfo {
+    exports college.student;
+}
+package college.student;
+
+public class Student {
+    private String name;
+    private int rollNo;
+
+    public Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public void display() {
+        System.out.println("Student: " + name + ", Roll No: " + rollNo);
+    }
+}
+
